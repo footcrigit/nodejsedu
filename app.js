48f68c8d36e77e11acdfadd61a0f14aba3831003
subhransupatra <footcri@gmail.com>
2019-01-01 23:56:25 -0500

    forth commit

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-var hold= 'abc';
-console.log(hold);
-console.log('hello subhransu');
+var express = require('express');
+var app = express();
+var port = 8080;
+app.get('/', function(req,res){
+  res.send('new mean proj test')
+});
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.get('/routing', function(req,res){
+  res.send('Mean project routing is working')
+});
+
+app.listen(port,function(err){
+  console.log('the server is running on ' + port);
+});
